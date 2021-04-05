46e8400bd32e2c13c5cb5ca25dae14e6dd55f2eb
Andre930 <969347529@qq.com>
2021-04-05 22:55:18 +0800

    极客，链表的学习笔记

---
+++ 极客_算法/chapter02/linked_list/linked_list_cycle/linked_list_cycle.go
@@ -0,0 +1,26 @@
+package main
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+// https://leetcode-cn.com/problems/linked-list-cycle/
+// 判断链表有环
+
+func hasCycle(head *ListNode) bool {
+
+	if head ==nil || head.Next==nil {
+		return false
+	}
+
+	slow ,fast :=head,head.Next
+	for fast!=slow {
+		if fast==nil||fast.Next==nil {
+			return false
+		}
+		slow=slow.Next
+		fast=fast.Next.Next
+	}
+	return true
+}
