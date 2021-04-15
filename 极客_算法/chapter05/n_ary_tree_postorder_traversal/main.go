5ab0ce505e34c3bf39b48383ce6f45a18863dab3
Andre930 <969347529@qq.com>
2021-04-15 23:17:52 +0800

    极客-算法 n叉树的后序遍历

---
+++ 极客_算法/chapter05/n_ary_tree_postorder_traversal/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "log"
+
+func main() {
+
+	root := NewNode(1)
+
+	firstChild := NewNode(3)
+	secondChild := NewNode(2)
+	threeChild := NewNode(4)
+	Child1 := NewNode(5)
+	Child2 := NewNode(6)
+	root.Children=append(root.Children,firstChild,secondChild,threeChild)
+	firstChild.Children=append(firstChild.Children,Child1,Child2)
+
+	//vals := postorder(root)
+	vals := postOrder(root)
+	log.Println(vals)
+}
---
+++ 极客_算法/chapter05/n_ary_tree_postorder_traversal/n_ary_tree_postorder_traversal.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+// https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal/
+// n 叉树的后序遍历
+
+func postorder(root *Node) []int {
+
+	res := []int{}
+	order(root, &res)
+	return res
+}
+
+func order(root *Node, res *[]int) {
+
+	// 递归终结条件
+	if root == nil {
+		return
+	}
+	// 当层循环 主体
+	for _, child := range root.Children {
+		order(child, res)
+	}
+	// 将当前val 追加到 res集合中
+	*res = append(*res, root.Val)
+
+}
+
+// 使用栈 解决
+func postOrder(root *Node) []int {
+	res := []int{}
+
+	if root == nil {
+		return res
+	}
+	stack := []*Node{}
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		stack = append(stack, root.Children...)
+	}
+	i, j := 0, len(res)-1
+	for i < j {
+		res[i], res[j] = res[j], res[i]
+		i++
+		j--
+	}
+	return res
+}
+
+func Print(root *Node) {
+
+	node := root
+	for node != nil {
+		fmt.Printf("%d-->\t", node.Val)
+
+	}
+}
+
+func NewNode(v int) *Node {
+	return &Node{
+		Val: v,
+	}
+
+}
