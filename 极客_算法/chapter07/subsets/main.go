13b96a3101d9d36a856476181e64bc671c046f64
Andre930 <969347529@qq.com>
2021-04-22 23:36:52 +0800

    极客-算法学习笔记  分治、回溯学习笔记

--- 极客_算法/chapter07/subsets/main.go
+++ 极客_算法/chapter07/subsets/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "log"
+
 func main() {
+
+
+	nums := []int{1,2,3}
+
+	res := subsets(nums)
+	log.Println(res)
 
 }
--- 极客_算法/chapter07/subsets/subsets.go
+++ 极客_算法/chapter07/subsets/subsets.go
@@ -1,1 +1,33 @@
 package main
+
+import "log"
+
+//  https://leetcode-cn.com/problems/subsets/
+
+// 子集  递归求解
+func subsets(nums []int) [][]int {
+
+	res :=[][]int{}
+	path :=[]int{}
+	length := len(nums)
+	dfs(0,length,path,nums,&res)
+	return res
+}
+
+func dfs(depth, length int, path, nums []int, res *[][]int) {
+
+	// 递归终止条件
+	if depth==length {
+		temp:=make([]int, len(path))
+		copy(temp,path)
+		log.Println(path)
+		*res=append(*res,path)
+		return
+	}
+	// 考虑选择当前位置元素
+	path=append(path,nums[depth])
+	dfs(depth+1,length,path,nums,res)
+	path=path[:len(path)-1]
+	// 不考虑当前位置元素
+	dfs(depth+1,length,path,nums,res)
+}
